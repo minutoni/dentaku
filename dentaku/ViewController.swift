3a6141cfc20809a9d8eaf0d243838a5bb66fbacf
所紀彦 <tokoronorihiko@ld11-tokoro-norihiko-no-macbook-air.local>
2019-04-03 22:56:59 +0900

    Initial Commit

---
+++ dentaku/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dentaku
+//
+//  Created by 所　紀彦 on 2019/04/03.
+//  Copyright © 2019年 所　紀彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
